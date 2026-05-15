69930674bee34a18e6d4d7e501ab7522b644ee69
AlbinDanielsson <adn20004@student.mdu.se>
2026-05-15 10:26:33 +0200

    Create fullPseudo.m

---
+++ fullPseudo.m
@@ -0,0 +1,15 @@
+%Set up 3 signals
+
+%loop forever
+
+    %Switch signal n (= 1, 2, or 3)
+
+    %pause
+    %trigg n
+    %pause
+
+    %record n
+
+    %process signal
+
+    %Update estimated position
