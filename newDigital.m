f931f50f66fdc98f1229bcc28f786eec12b7ff53
AlbinDanielsson <adn20004@student.mdu.se>
2026-05-19 12:41:37 +0200

    test

--- newDigital.m
+++ newDigital.m
@@ -5,28 +5,35 @@ myDaq = daq("ni");
 % Digital trigger output
 addoutput(myDaq, "myDAQ1", "port0/line0", "Digital");
 
-%Digital input
+% Digital input
 addinput(myDaq, "myDAQ1", "port0/line1", "Digital");
 
-
+% Initialize output LOW
 write(myDaq, false);
 
 while true
     pause(1);
 
+    % Set output HIGH
     write(myDaq, true);
 
-    % Read input
-    in = read(myDaq, "OutputFormat", "logical");
-    disp("Input state: " + in)
+    % Read input (matrix format for older MATLAB versions)
+    in = read(myDaq, "OutputFormat", "matrix");
+    inputState = in(1);  % first input channel
+
+    disp("Input state: " + inputState);
 
     pause(1);
 
+    % Set output LOW
     write(myDaq, false);
 
     % Read again
-    in = read(myDaq, "OutputFormat", "logical");
-    disp("Input state: " + in);
+    in = read(myDaq, "OutputFormat", "matrix");
+    inputState = in(1);
+
+    disp("Input state: " + inputState);
 end
 
+% This line will never be reached, but kept for safety
 write(myDaq, false);
