9009a3de6e7d0634f249024bcc467adcdf09d9f5
AlbinDanielsson <adn20004@student.mdu.se>
2026-05-19 13:29:06 +0200

    much faster

--- newDigital.m
+++ newDigital.m
@@ -12,18 +12,25 @@ addinput(myDaq, "myDAQ1", "port0/line1", "Digital");
 addinput(myDaq, "myDAQ1", "port0/line4", "Digital");
 addinput(myDaq, "myDAQ1", "port0/line7", "Digital");
 
-duration = 150;
-pauseLen = 1;
+idleLen = 30;
+pauseLen = 0.00001;
 
-triggerOne = repmat([1 0 0], 1, 50);
-triggerTwo = repmat([0 1 0], 1, 50);
-triggerThree = repmat([0 0 1], 1, 50);
+idle = zeros(1, idleLen);
 
-for i = 1:1:duration
-    pause(pauseLen);
+triggerOne = repmat(idle, 1, 3);
+triggerOne(1) = 1;
+triggerTwo = repmat(idle, 1, 3);
+triggerTwo(idleLen + 1) = 1;
+triggerThree = repmat(idle, 1, 3);
+triggerTwo(2 * idleLen + 1) = 1;
 
-    write(myDaq, [triggerOne(i), triggerTwo(i), triggerThree(i)]);
+while true
+    for i = 1:1:numel(triggerOne)
+        pause(pauseLen);
 
-    in = read(myDaq, "OutputFormat", "matrix");
-    fprintf("Input state: %.1f %.1f %.1f \n", in(1), in(2), in(3));
+        write(myDaq, [triggerOne(i), triggerTwo(i), triggerThree(i)]);
+
+        in = read(myDaq, "OutputFormat", "matrix");
+        fprintf("Input state: %.1f %.1f %.1f \n", in(1), in(2), in(3));
+    end
 end
