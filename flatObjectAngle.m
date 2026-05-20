9cddf71d27c6c656019f9511315f571c4fae49ab
AlbinDanielsson <adn20004@student.mdu.se>
2026-05-20 14:51:32 +0200

    flat

--- flatObjectAngle.m
+++ flatObjectAngle.m
@@ -1,4 +1,6 @@
 function a = flatObjectAngle(r1, r2, l)
-    a = atan2(r2, r1) * (180 / pi); % Calculate the angle in degrees
+    %Assume r1 is to the systems right
+    delta_r = r1 - r2;
+    a = atan2(delta_r, l);
 end
 
--- test1.m
+++ test1.m
@@ -1,5 +1,9 @@
 clear all
 clc
 
-a = 1;
-b = plusOne(a);
+r1 = 30;
+r2 = 20;
+l = 5;
+
+angle = flatObjectAngle(r1, r2, l);
+angle = angle * (180 / pi);
